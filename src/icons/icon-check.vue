934162f24d1bdde98ed70b0069739e0e0474803c
Juan de la Cruz <juan.delacruz@kaleidos.net>
2018-12-19 09:45:01 +0100

    add icons

---
+++ src/icons/icon-check.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 0C8.95 0 0 8.95 0 20C0 31.04 8.95 40 20 40C31.04 40 40 31.04 40 20C40 8.95 31.04 0 20 0ZM16 30L6 20L8.83 17.17L16 24.34L31.17 9.17L34 12L16 30Z"/>
+    </svg>
+</template>
+
+<script>
+export default {
+    props: {
+        className: String
+    }
+}
+</script>
