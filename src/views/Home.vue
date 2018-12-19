0f148d420b30adc4a53e710bb5f673cc63429b11
Yamila Moreno <yamila.ms@gmail.com>
2018-12-19 10:50:07 +0100

    Home with fridge

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="home">
     <Header></Header>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Nevera</h2>
+    <span>Nombre: {{ name1 }}</span> -- <span>Avísame: {{ notifyMe1 }}</span>
+    <br />
+    <span>Nombre: {{ name2 }}</span> -- <span>Avísame: {{ notifyMe2 }}</span>
+    <br />
+    <span>Nombre: {{ name3 }}</span> -- <span>Avísame: {{ notifyMe3 }}</span>
+
+    <br />
+    <h2>Congelador</h2>
+    <span>Tuppers en la nevera: {{ tuppersCount }}</span> -- <span>Huecos libres: {{ spaceLeft }}</span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import Header from '@/components/Header.vue'
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    HelloWorld,
     Header
+  },
+  data: function () {
+    return {
+      name1: 'Sopa',
+      notifyMe1: '2018-12-19',
+      name2: 'Albóndigas',
+      notifyMe2: '2018-12-20',
+      name3: 'Bonito con tomate',
+      notifyMe3: '2018-12-20',
+      tuppersCount: 14,
+      spaceLeft: 2
+    }
   }
 }
 </script>
